6802feb8d8f8d9caf0de97f9c373fbaa25eb6022
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-09-19 17:56:40 -0600

    Update code.js

--- code.js
+++ code.js
@@ -12,6 +12,6 @@ function sum(x, hi, low,){
     return lowSum + midSum + hiSum;
 }
 function divideAndConquerSum(a) {
-    if (a.length === 0) return 0;  // Handle empty array
+    if (a.length === 0) return 0;  
     return sum(a, 0, a.length - 1);
 }
