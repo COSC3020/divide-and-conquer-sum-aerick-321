f0f4d7fe951535e392de3e14b13b8e712a1c5ae0
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-09-19 17:50:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
+function sum(x, hi, low,){
+    if (low === hi) {
+        return x[low];
+    }
+    var mid1 = Math.floor((low+hi)/3);
+    var mid2 = Math.floor(2 *(low+hi)/3);
+
+    var lowSum = sum(x, low, mid1);
+    var midSum= sum(x, mid1 + 1, mid2);
+    var hiSum = sum (x, mid2 +1, hi);
+
+    return lowSum + midSum + hiSum;
+}
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length === 0) return 0;  // Handle empty array
+    return sum(a, 0, a.length - 1);
 }
